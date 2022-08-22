39eae07240bd94a4dd020ec1e650c9cc595d9039
Arif <kanebrian0634@gmail.com>
2022-08-22 13:56:59 +0300

    ...

--- 005_Models_1_s02/fscohort/views.py
+++ 005_Models_1_s02/fscohort/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome backend")
+    return HttpResponse("<h1>Welcome backend</h1>")
--- 005_Models_1_s02/main/urls.py
+++ 005_Models_1_s02/main/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("fscohort.urls")),
+    path('fscohort/', include("fscohort.urls")),
 ]
+
