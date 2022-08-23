941683d6f774ae1f4972639f36c77f8e19a21884
Arif <kanebrian0634@gmail.com>
2022-08-23 15:04:57 +0300

    ....

--- 006_Models_2_s02/relations/admin.py
+++ 006_Models_2_s02/relations/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Creator, Language
+from .models import Creator, Developer, Framework, Language
 
 admin.site.register(Creator)
 admin.site.register(Language)
+admin.site.register(Framework)
+admin.site.register(Developer)
