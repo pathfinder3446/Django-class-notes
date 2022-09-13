ca4c15abe4778f510d459eb8f44c76d4cb65348e
Arif <kanebrian0634@gmail.com>
2022-09-13 15:01:33 +0300

    ..

--- 014_Django_Test/hashing/tests.py
+++ 014_Django_Test/hashing/tests.py
@@ -42,27 +42,27 @@ class UnitTestCase(TestCase):
         self.assertTrue(form.is_valid())
         
     
-    def test_hash_func_works(self):
-        text_hash = hashlib.sha256('hello'.encode('utf-8')).hexdigest()
-        self.assertEqual('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', text_hash)
+#     def test_hash_func_works(self):
+#         text_hash = hashlib.sha256('hello'.encode('utf-8')).hexdigest()
+#         self.assertEqual('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', text_hash)
         
         
-    def test_hash_object(self):
-        hash = Hash()
-        hash.text = "hello"
-        hash.hash = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
-        hash.save()
+#     def test_hash_object(self):
+#         hash = Hash()
+#         hash.text = "hello"
+#         hash.hash = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+#         hash.save()
         
-        pulled_hash = Hash.objects.get(hash='2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
+#         pulled_hash = Hash.objects.get(hash='2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
         
-        self.assertEqual(hash.text, pulled_hash.text)
+#         self.assertEqual(hash.text, pulled_hash.text)
         
     
-    def test_viewing_hash(self):
-        hash = Hash()
-        hash.text = "hello"
-        hash.hash = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
-        hash.save()
-        response = self.client.get('/hash/2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
+#     def test_viewing_hash(self):
+#         hash = Hash()
+#         hash.text = "hello"
+#         hash.hash = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+#         hash.save()
+#         response = self.client.get('/hash/2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
         
-        self.assertContains(response, "hello")
+#         self.assertContains(response, "hello")
